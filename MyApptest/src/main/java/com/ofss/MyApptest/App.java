9489bcc5ccb036b7b0da6c978fdb9af422087bbd
kishore <kishore.shivanandan@oracle.com>
2020-06-24 11:40:26 +0530

    first eclipse commit

---
+++ MyApptest/src/main/java/com/ofss/MyApptest/App.java
@@ -0,0 +1,16 @@
+package com.ofss.MyApptest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public static void main( String[] args )
+	    {
+	    	Calculator c=new Calculator();
+	    	
+	        System.out.println( c.add(10,20));
+	        System.out.println( c.multiply(11,9));
+	    }
+}
