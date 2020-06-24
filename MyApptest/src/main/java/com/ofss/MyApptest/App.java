d17adcc3db8c8baa4a74eef3874d905ffecdc041
kishore <kishore.shivanandan@oracle.com>
2020-06-24 12:42:14 +0530

    new cusotmer insert

--- MyApptest/src/main/java/com/ofss/MyApptest/App.java
+++ MyApptest/src/main/java/com/ofss/MyApptest/App.java
@@ -9,8 +9,9 @@ public class App
 	 public static void main( String[] args )
 	    {
 	    	Calculator c=new Calculator();
-	    	
+	    	customer c1=new customer();
 	        System.out.println( c.add(10,20));
 	        System.out.println( c.multiply(11,9));
+	        System.out.println(c1.custname());
 	    }
 }
